0605d45a4508456f7d9398690db14edc06bb2b2e
Georgiana <georgiana.marti.gg@gmail.com>
2023-11-03 15:57:48 +0200

    Save and load system

--- Stf Unity/Assets/IdleGame.cs
+++ Stf Unity/Assets/IdleGame.cs
@@ -4,14 +4,10 @@ using UnityEngine.UI;
 public class IdleGame : MonoBehaviour
 {
     public Text dropNumberText;
-    
-
     public Text dropsPerSecondText;
     public Text rainText;
     public Text bucketUpgradeText;
 
-    //public double dropsPerSecondRain; // for button Rain
-    //public double bucketUpgradeDrop; // for button bucket update
     public double drops;
     // what changes in the buttons' text
     public double rainPower; // dropsPerSeconds
@@ -24,11 +20,29 @@ public class IdleGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        drops = 0;
-        rainPower = 0;
-        bucketUpgradePower = 1;
+        //PlayerPrefs.DeleteAll();
         InvokeRepeating("IncrementDrops", 1.0f, 1.0f); // Calls IncrementDrops every 1 second.
+        InvokeRepeating("Save", 1.0f, 1.0f); // Calls IncrementDrops every 1 second.
+        Load();
+    }
 
+    public void Load()
+    {
+        drops = double.Parse(PlayerPrefs.GetString("drops","0"));
+        rainPower = double.Parse(PlayerPrefs.GetString("rainPower","0"));
+        bucketUpgradePower = double.Parse(PlayerPrefs.GetString("bucketUpgradePower","1"));
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetString("drops", drops.ToString());
+        PlayerPrefs.SetString("rainPower", rainPower.ToString());
+        PlayerPrefs.SetString("bucketUpgradePower", bucketUpgradePower.ToString());
+    }
+
+    public void ResetPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll(); // This clears all PlayerPrefs data.
     }
 
     // Update is called once per frame
@@ -38,27 +52,33 @@ public class IdleGame : MonoBehaviour
         dropsPerSecondText.text = rainPower + "/sec";
         rainText.text = "Rain\n" + rainPower + " / sec";
         bucketUpgradeText.text = "Bucket Upgrade\n" + bucketUpgradePower + " / tap";
-        //drops += rainPower * Time.deltaTime;
-        //InvokeRepeating("IncrementDrops", 10.0f, 100.0f); // Calls IncrementDrops every 1 second.
+        
     }
 
     //Buttons
-    public void Clicked(){
+    public void Clicked()
+    {
         drops += bucketUpgradePower;
     }
 
-    public void RainClicked(){
-        if (!isRainActive){
+    public void RainClicked()
+    {
+        if (!isRainActive)
+        {
             rainPower += 5;
-            isRainActive = true;
+            isRainActive = false;
         }
     }
 
-    private void IncrementDrops(){
+    private void IncrementDrops()
+    {
         drops += rainPower;
     }
 
-    public void BucketUpgradeClicked(){
+    public void BucketUpgradeClicked()
+    {
         bucketUpgradePower += 1;
     }
+
+   
 }
