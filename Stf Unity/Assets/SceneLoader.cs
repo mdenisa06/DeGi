780ebd8a06e9fc0d343c55bb194cca677e51c71c
Mita-Mihaela Denisa <mihaela.mita@student.upt.ro>
2023-11-10 15:56:04 +0200

    Add Daily Quest Scene

--- Stf Unity/Assets/SceneLoader.cs
+++ Stf Unity/Assets/SceneLoader.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class SceneLoader : MonoBehaviour
 {
+    int currentSceneIndex;
     public void LoadNextScene()
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
-    public void LoadStartScene()
+    public void LoadPreviousScene()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 }
